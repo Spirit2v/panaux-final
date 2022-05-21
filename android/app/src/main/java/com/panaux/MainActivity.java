6b798955f7b309863a690f8844b6a0e90c6d17f5
Suyash Srivastava <55198615+Spirit2v@users.noreply.github.com>
2022-05-21 21:56:46 +0530

    final

--- android/app/src/main/java/com/panaux/MainActivity.java
+++ android/app/src/main/java/com/panaux/MainActivity.java
@@ -3,13 +3,19 @@ package com.panaux;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-
+import org.devio.rn.splashscreen.SplashScreen; // add this
+import android.os.Bundle; // add this
 public class MainActivity extends ReactActivity {
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    } 
   @Override
   protected String getMainComponentName() {
     return "panaux";
